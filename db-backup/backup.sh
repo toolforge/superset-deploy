4975adf52fcd93218824c71a928cbc5c8612e5d4
Vivian Rook <91909295+vivian-rook@users.noreply.github.com>
2023-12-12 19:51:33 -0500

    Only rotate backups on success (#13)
    Bug: T352766

--- db-backup/backup.sh
+++ db-backup/backup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-mv ./superset-db-backup.4 ./superset-db-backup.5
-mv ./superset-db-backup.3 ./superset-db-backup.4
-mv ./superset-db-backup.2 ./superset-db-backup.3
-mv ./superset-db-backup.1 ./superset-db-backup.2
-mv ./superset-db-backup ./superset-db-backup.1
+set -e
 
 kubectl cp ./pg_dump.sh default/superset-postgresql-0:/tmp/pg_dump.sh
 kubectl exec -it pod/superset-postgresql-0 -- bash /tmp/pg_dump.sh
 kubectl cp default/superset-postgresql-0:tmp/db.tar ./superset-db-backup
 
+mv ./superset-db-backup.4 ./superset-db-backup.5
+mv ./superset-db-backup.3 ./superset-db-backup.4
+mv ./superset-db-backup.2 ./superset-db-backup.3
+mv ./superset-db-backup.1 ./superset-db-backup.2
+mv ./superset-db-backup ./superset-db-backup.1
