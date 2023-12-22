1c7262f40bc6aa73d242cb598d33df7f2eeac138
Vivian Rook <vrook@wikimedia.org>
2023-12-22 11:06:14 -0500

    absolute path

--- db-backup/backup.sh
+++ db-backup/backup.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-kubectl cp ./pg_dump.sh default/superset-postgresql-0:/tmp/pg_dump.sh
-kubectl exec -it pod/superset-postgresql-0 -- bash /tmp/pg_dump.sh
-kubectl cp default/superset-postgresql-0:tmp/db.tar ./superset-db-backup
+/usr/local/bin/kubectl cp ./pg_dump.sh default/superset-postgresql-0:/tmp/pg_dump.sh
+/usr/local/bin/kubectl exec -it pod/superset-postgresql-0 -- bash /tmp/pg_dump.sh
+/usr/local/bin/kubectl cp default/superset-postgresql-0:tmp/db.tar ./superset-db-backup
 
 mv ./superset-db-backup.4 ./superset-db-backup.5
 mv ./superset-db-backup.3 ./superset-db-backup.4
