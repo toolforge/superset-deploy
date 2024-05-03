2177c624c13f598f8834f5000b699af3f710f1d7
Vivian Rook <vrook@wikimedia.org>
2024-05-03 12:10:48 -0400

    Update credential

--- tofu/vars.tf
+++ tofu/vars.tf
@@ -30,7 +30,7 @@ variable "application_credential_id" {
   type = map(any)
   default = {
     "codfw1dev" = "34e6012e4e0649e192b3b95dabb1621c"
-    "eqiad1"    = "23d2deb4328447678b276de7516370c4"
+    "eqiad1"    = "432c786de65a4fb58a7fe8f9a4e29d12"
   }
 }
 
