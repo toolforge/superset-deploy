081682080e5027ac379f11b54e8bda1b07d1bcf6
Vivian Rook <vrook@wikimedia.org>
2024-06-20 11:29:12 -0400

    update to g4 flavors
    Bug: T368076

--- tofu/127.tf
+++ tofu/127.tf
@@ -1,12 +1,17 @@
-resource "openstack_containerinfra_cluster_v1" "k8s_126_2" {
-  name                = "superset${var.name[var.datacenter]}-126-2"
-  cluster_template_id = resource.openstack_containerinfra_clustertemplate_v1.template_126_2.id
+resource "openstack_containerinfra_cluster_v1" "k8s_127" {
+  name                = "superset${var.name[var.datacenter]}-127"
+  cluster_template_id = resource.openstack_containerinfra_clustertemplate_v1.template_127.id
   master_count        = 1
   node_count          = var.workers[var.datacenter]
 }
 
-resource "openstack_containerinfra_clustertemplate_v1" "template_126_2" {
-  name                  = "superset${var.name[var.datacenter]}-126-2"
+resource "local_file" "kube_config" {
+  content  = resource.openstack_containerinfra_cluster_v1.k8s_127.kubeconfig.raw_config
+  filename = "kube.config"
+}
+
+resource "openstack_containerinfra_clustertemplate_v1" "template_127" {
+  name                  = "superset${var.name[var.datacenter]}-127"
   coe                   = "kubernetes"
   dns_nameserver        = "8.8.8.8"
   docker_storage_driver = "overlay2"
@@ -14,10 +19,10 @@ resource "openstack_containerinfra_clustertemplate_v1" "template_126_2" {
   external_network_id   = var.external_network_id[var.datacenter]
   fixed_subnet          = var.fixed_subnet[var.datacenter]
   fixed_network         = var.fixed_network[var.datacenter]
-  flavor                = "g3.cores2.ram4.disk20"
+  flavor                = var.worker_flavor[var.datacenter]
   floating_ip_enabled   = "false"
   image                 = "Fedora-CoreOS-38"
-  master_flavor         = "g3.cores2.ram4.disk20"
+  master_flavor         = var.control_flavor[var.datacenter]
   network_driver        = "flannel"
 
   labels = {
